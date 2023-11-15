11e69c63610c73284b4f7ecccf5079beb28bd0b7
BartoszSzubski <szubscy@gmail.com>
2023-11-15 18:14:11 +0100

    logout section plus hide password added

--- script.js
+++ script.js
@@ -27,6 +27,20 @@ forgotLink.addEventListener("click", () => {
   loginForm.classList.add("form__section--active");
   forgotForm.classList.remove("form__section--active");
 });
+//logged in section
+const loggedForm = document.querySelector(".logged");
+const loggedLink = document.querySelector(".logged-link");
+const buttonLogout = document.querySelector(".button__logout");
+
+loggedLink.addEventListener("click", () => {
+  loggedForm.classList.add("form__section--active");
+  loginForm.classList.remove("form__section--active");
+});
+
+buttonLogout.addEventListener("click", () => {
+  loggedForm.classList.remove("form__section--active");
+  loginForm.classList.add("form__section--active");
+});
 
 //terms of use section
 const popupTerms = document.querySelector(".popup__terms__section");
@@ -65,7 +79,7 @@ form.addEventListener("submit", (e) => {
   validateInputs();
 });
 
-// login validation
+// login validation settings
 const showError = (element, message) => {
   const inputControl = element.parentElement;
   const errorDisplay = inputControl.querySelector(".error");
@@ -114,6 +128,20 @@ const validatePasswordInput = () => {
 showSuccess(user, "");
 showSuccess(password, "");
 
+//hide and show password section
+const eyeIcon = document.querySelector(".eye-icon");
+const eyeOpen = document.querySelector(".eye-open");
+const eyeClose = document.querySelector(".eye-close");
+//id password
+eyeIcon.addEventListener("click", () => {
+  if (password.type === "password") {
+    password.type = "text";
+    eyeIcon.src = "images/eye-open.png";
+  } else {
+    password.type = "password";
+    eyeIcon.src = "images/eye-close.png";
+  }
+});
 //forgot password validation section
 
 const newPassword = document.getElementById("newpassword");
@@ -157,4 +185,91 @@ const validatePasswordsMatch = (isSubmit = false) => {
 
 showSuccess(confirmPasswordError, "");
 
-//register validation section
+// register validation section
+const userRegister = document.getElementById("userregister");
+const email = document.getElementById("email");
+const passwordRegister = document.getElementById("passwordregister");
+const buttonRegister = document.querySelector(".button__register");
+
+userRegister.addEventListener("blur", () => {
+  validateUserRegisterInput();
+});
+
+email.addEventListener("blur", () => {
+  validateEmailInput();
+});
+
+passwordRegister.addEventListener("blur", () => {
+  validatePasswordRegisterInput();
+});
+
+const validateEmailInput = () => {
+  const emailValue = email.value.trim();
+
+  if (emailValue === "") {
+    showError(email, "Email is required");
+  } else if (!isValidEmail(emailValue)) {
+    showError(email, "Invalid email address");
+  } else {
+    showSuccess(email, "");
+  }
+};
+
+const validatePasswordRegisterInput = (isSubmit = false) => {
+  const passwordValue = passwordRegister.value.trim();
+
+  if (passwordValue === "") {
+    showError(passwordRegister, "Password is required");
+  } else if (passwordValue.length < 8) {
+    showError(passwordRegister, "Password must contain at least 8 characters");
+  } else if (!/[`~!@#$%^&*()_+-=;:'"|{}.>,</?]/.test(passwordValue)) {
+    showError(passwordRegister, "Minimum one special character required");
+  } else if (!/\d/.test(passwordValue)) {
+    showError(passwordRegister, "Password must contain at least one digit");
+  } else {
+    showSuccess(passwordRegister, "");
+    if (isSubmit) {
+      registerForm.classList.remove("form__section--active");
+      loginForm.classList.add("form__section--active");
+    }
+  }
+};
+
+const isValidEmail = (email, isSubmit = false) => {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+};
+
+const validateUserRegisterInput = (isSubmit = false) => {
+  const userValue = userRegister.value.trim();
+
+  if (userValue === "") {
+    showError(userRegister, "Username is required");
+  } else if (userValue.length < 6) {
+    showError(userRegister, "Username must be at least 6 characters");
+  } else {
+    showSuccess(userRegister, "");
+    if (isSubmit) {
+      validateRegistrationForm();
+    }
+  }
+};
+
+showSuccess(userRegister, "");
+showSuccess(email, "");
+showSuccess(passwordRegister, "");
+
+const validateRegistrationForm = () => {
+  const isPasswordValid = validatePasswordRegisterInput(true);
+  const isEmailValid = isValidEmail(email.value.trim(), true);
+  const isUserValid = validateUserRegisterInput(true);
+
+  if (isPasswordValid && isEmailValid && isUserValid) {
+    registerForm.classList.remove("form__section--active");
+    loginForm.classList.add("form__section--active");
+  }
+};
+
+buttonRegister.addEventListener("click", () => {
+  validateRegistrationForm();
+});
